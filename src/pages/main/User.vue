636cf8e3ace784f249eed2945deabbf40c72feff
DahengZhang <Daheng_Zhang@outlook.com>
2020-02-12 16:44:55 +0800

    内置方法

--- src/pages/main/User.vue
+++ src/pages/main/User.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="user-page">
-        yonghu
+        <el-button @click="api()" type="primary">请求</el-button>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'User',
+    methods: {
+        api () {
+            console.log(this.moment().format('YYYY/MM/DD HH:mm:ss'))
+            this.axios.get('/article')
+        }
+    }
+}
+</script>
